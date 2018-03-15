0c0f960fa67028c28ec77ffbd40ecbdee0075d3c
Goncharuk Yaroslav <gonyarik@gmail.com>
2018-03-15 22:31:01 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,82 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expression = [];
+    this.expression.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.expression.push("+");
+    this.expression.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.expression.push("-");
+    this.expression.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.expression.push("*");
+    this.expression.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.expression.push("/");
+    this.expression.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.expression.push("^");
+    this.expression.push(number);
+    return this;
+  }
+
+  valueOf() {
+    let arr = this.expression;
+    console.log(arr);
+    let index = arr.lastIndexOf("^");
+    while (index > 0) {
+      arr.splice(index - 1, 3, Math.pow(arr[index -1] , arr[index + 1])); 
+      index = arr.lastIndexOf("^");
+      console.log(arr);
+    }
+    
+    index = arr.indexOf("/");
+    while (index > 0) {
+      arr.splice(index - 1, 3, Math.floor(arr[index -1] / arr[index + 1])); 
+      index = arr.indexOf("/");
+      console.log(arr);
+    }
+
+    index = arr.indexOf("*");
+    while (index > 0) {
+      arr.splice(index - 1, 3, arr[index -1] * arr[index + 1]); 
+      index = arr.indexOf("*");
+      console.log(arr);
+    }
+
+    
+
+    index = arr.indexOf("-");
+    while (index > 0) {
+      arr.splice(index - 1, 3, arr[index -1] - arr[index + 1]); 
+      index = arr.indexOf("-");
+      console.log(arr);
+    }
+
+    index = arr.indexOf("+");
+    while (index > 0) {
+      arr.splice(index - 1, 3, arr[index -1] + arr[index + 1]); 
+      index = arr.indexOf("+");
+      console.log(arr);
+    }
+
+    return arr[0];
+
+
   }
 }
 
